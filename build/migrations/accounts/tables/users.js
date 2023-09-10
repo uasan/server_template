a845e30ce3e8935b58f44aa81de00e8a0c549dc3
uasan <ua.san.alex@gmail.com>
2023-09-10 20:13:47 +0200

    WIP

--- build/migrations/accounts/tables/users.js
+++ build/migrations/accounts/tables/users.js
@@ -2,8 +2,5 @@ import { Migration } from "../../../lib/Migration.js";
 export class UsersTable extends Migration {
     static path = "accounts/tables/users";
     async up() {
-        await this.sql `CREATE TABLE accounts.users (
-
-    )`;
     }
 }
--- src/migrations/accounts/tables/users.ts
+++ src/migrations/accounts/tables/users.ts
@@ -2,8 +2,7 @@ import { Migration } from '../../../lib/Migration';
 
 export class UsersTable extends Migration {
   async up() {
-    await this.sql`CREATE TABLE accounts.users (
-
-    )`;
+    // await this.sql`CREATE TABLE public.users (
+    // )`;
   }
 }
