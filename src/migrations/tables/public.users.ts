17dedcc3e388bc101985973c1e74efc5c0ecd39e
uasan <ua.san.alex@gmail.com>
2025-06-24 22:48:39 +0200

    Implement version migrate

--- src/migrations/tables/public.users.ts
+++ src/migrations/tables/public.users.ts
@@ -1,8 +1,6 @@
 import { Migration } from '#lib/context/Migration';
 
 export class UsersTable extends Migration {
-  static version = 'AAAAAA';
-
   async up() {
     await this.sql`CREATE TABLE public.users (
       uid uuid primary key 
